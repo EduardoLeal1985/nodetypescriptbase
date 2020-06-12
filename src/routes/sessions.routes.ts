b1e3380325baa73e4aa87b14589204e1b205ec83
EduardoLeal1985 <eduleal2005@gmail.com>
2020-06-12 11:31:34 -0300

    Aula 76 - Gerando Token JWT

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,12 +10,12 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authUser = new AuthenticateUserService();
 
-    const { user } = await authUser.execute({
+    const { user, token } = await authUser.execute({
       email,
       password,
     });
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
