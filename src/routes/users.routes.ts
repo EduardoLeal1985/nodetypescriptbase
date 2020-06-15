dd42461f8b4bb4ea45062bd1f20f525e1c1fdfb2
EduardoLeal1985 <eduleal2005@gmail.com>
2020-06-15 07:02:35 -0300

    Aula 79 e 80 - Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,7 +3,9 @@ import multer from 'multer';
 
 import uploadConfig from '../config/upload';
 
-import CreateUsersService from '../services/CreateUserService';
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -13,7 +15,7 @@ usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
 
-    const createUser = new CreateUsersService();
+    const createUser = new CreateUserService();
 
     const user = await createUser.execute({
       name,
@@ -34,8 +36,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: 'true' });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
