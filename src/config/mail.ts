7c8dfa4ed424ceddcc5e2057314174a39287be8b
EduardoLeal1985 <eduleal2005@gmail.com>
2020-08-22 13:25:43 -0300

    Aula 187 - Organizando o container

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'eduleal2005@gmail.com',
+      name: 'Eduardo da GoBarber',
+    },
+  },
+} as IMailConfig;
