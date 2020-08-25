cccd7f970324fb1fb033ed9e6d02ee9fdaa10b99
EduardoLeal1985 <eduleal2005@gmail.com>
2020-08-25 11:19:20 -0300

    Aula 190, 191 - add e remove cache

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -1,7 +1,6 @@
-import { stringList } from 'aws-sdk/clients/datapipeline';
-
 export default interface ICacheProvider {
-  save(key: string, value: string): Promise<void>;
-  recover(key: string): Promise<string | null>;
+  save(key: string, value: any): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
