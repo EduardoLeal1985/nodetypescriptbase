3e86428649a6ce251713dbada6f3b1a42672d6b9
EduardoLeal1985 <eduleal2005@gmail.com>
2020-08-25 09:56:23 -0300

    Aula 189 - Configurando cache e redis

---
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -0,0 +1,7 @@
+import { stringList } from 'aws-sdk/clients/datapipeline';
+
+export default interface ICacheProvider {
+  save(key: string, value: string): Promise<void>;
+  recover(key: string): Promise<string | null>;
+  invalidate(key: string): Promise<void>;
+}
